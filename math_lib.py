4aac25f728beb0d1b7caac69af1ea186493c9596
Lin Wang <victoongo@gmail.com>
2023-12-03 17:06:08 -0800

    starter code

---
+++ math_lib.py
@@ -0,0 +1,31 @@
+def min(lst):
+    pass
+
+
+def max(lst):
+    pass
+
+
+def mean(lst):
+    pass
+
+
+def median(lst):
+    pass
+
+
+def variance(lst):
+    pass
+
+
+def std(lst):
+    pass
+
+
+list1 = [3, 56, 8, 2, 90]
+print(min(list1))
+print(max(list1))
+print(mean(list1))
+print(median(list1))
+print(variance(list1))
+print(std(list1))
